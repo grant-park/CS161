356bd654e8861568aed42f2544c33be5309dc36c
Hunter Voegele <hvoegele18@amherst.edu>
2015-12-07 23:59:10 -0500

    ControlUnit
    Implementing a case statement that will work as the control unit. It
    will sadly break the project as of right now because I haven’t figured
    out all the cases which is why it is a branch until I have done so.

--- controlUnit.sv
+++ controlUnit.sv
@@ -1,10 +1,38 @@
 
 module controlUnit(input logic  [5:0] op,
 
+	output logic MemtoReg,
+	output logic Branch,
+	output logic [2:0]ALUControl,
+	output logic ALUSrc,
+	output logic RegDst,
 	output logic memwrite,
 	output logic regwrite);
 
-   assign memwrite = op[0] & op[1] & ~op[2] & op[3] & ~op[4] & op[5];
-	assign regwrite = op[0] & op[1] & ~op[2] & ~op[3] & ~op[4] & op[5];
+   always_comb
+	 	case(op)
+		//Add - Need to figure out how to indicate the opcodes for cases
+		6'b100000:
+		//lw
+		6'b100011:
+		//sw
+		6'b101011:
+		//jr
+		6'b001000:
+		//jal
+		6'b000011:
+		/xor
+		6'b100110:
+		//xori
+		6'b001110:
+		//neg
+		6'b000100:
+		//beqz
+		6'b010000:
+		//sll
+		6'b000000:
+		/srl
+		6'b000010:
+
 
 	endmodule
